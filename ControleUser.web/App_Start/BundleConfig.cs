ba2330e3572efaee9e1670a6a0a3764221792796
João Batista Ramos de Carvalho <j.batistacarvalho95@gmail.com>
2021-03-07 23:01:17 -0300

    ajuste de grupo de produtos

--- ControleUser.web/App_Start/BundleConfig.cs
+++ ControleUser.web/App_Start/BundleConfig.cs
@@ -20,9 +20,12 @@ namespace ControleUser.web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.min.js"));
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
