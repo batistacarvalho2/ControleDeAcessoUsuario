2cab81f6f15c14ed47b6ea80fe66a49abc052038
João Batista Ramos de Carvalho <j.batistacarvalho95@gmail.com>
2021-03-15 22:39:44 -0300

    ajuste no projeto

---
+++ ControleUser.web/Models/UsuarioModel.cs
@@ -0,0 +1,39 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ControleUser.web.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(string login, string senha)
+        {
+            var ret = false;
+           
+            using (var conexao = new NpgsqlConnection())
+            {
+                conexao.ConnectionString = "server = localhost; user id = postgres; password = 123; database = postgres";
+                conexao.Open();
+              
+                NpgsqlCommand comando = new NpgsqlCommand(string.Format("select count(*) from usuario where login='{0}' and senha='{1}'", login,senha), conexao);
+
+                try
+                {
+                    ret = ((long)comando.ExecuteScalar() > 0);
+                 
+                }
+                catch (NpgsqlException e)
+                {
+                    Console.WriteLine("Erro " + e);
+                }
+                conexao.Close();
+
+            }
+            return ret;
+        }
+    }
+}
+
