372f8eba43273e1f666d94797e92273de842cd03
João Batista Ramos de Carvalho <j.batistacarvalho95@gmail.com>
2021-03-16 21:52:05 -0300

    commit 2021-03-16:21h51
    Configuração de criptografia md5

--- ControleUser.web/Models/UsuarioModel.cs
+++ ControleUser.web/Models/UsuarioModel.cs
@@ -1,6 +1,8 @@
-﻿using Npgsql;
+﻿using ControleUser.web.Helpers;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -15,10 +17,10 @@ namespace ControleUser.web.Models
            
             using (var conexao = new NpgsqlConnection())
             {
-                conexao.ConnectionString = "server = localhost; user id = postgres; password = 123; database = postgres";
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
                 conexao.Open();
-              
-                NpgsqlCommand comando = new NpgsqlCommand(string.Format("select count(*) from usuario where login='{0}' and senha='{1}'", login,senha), conexao);
+
+                NpgsqlCommand comando = new NpgsqlCommand(string.Format("select count(*) from usuario where login='{0}' and senha='{1}'", login, CriptoHelper.HashMD5(senha)), conexao);
 
                 try
                 {
